f8e4f1f6ba169eb0197b642458874198a435383a
muhammedklk <muhammedklkm@gmail.com>
2026-08-01 08:52:44 +0530

    feat: initialize global CSS design system and create product details page structure

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -147,7 +147,7 @@ const ProductDetails = () => {
     }
 
     return (
-        <main className="product-detail-main" style={{ paddingTop: '105px' }}>
+        <main className="product-detail-main" style={{ paddingTop: '85px' }}>
             {/* Breadcrumbs */}
             <div className="container breadcrumbs-container">
                 <span className="breadcrumb-item"><Link to="/">Home</Link></span>
